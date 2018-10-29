7ca8a40316a308f30a27cc818dae5c098f793096
Ricardo Graça <ricardo@devius.net>
2018-10-29 23:35:53 +0000

    Add navbar height offset when scrolling to anchor

---
+++ static/scripts/main.js
@@ -0,0 +1,11 @@
+(function() {
+  var navbarHeight = document.querySelector('.main-navbar').offsetHeight
+
+  function scrollWithOffset() {
+    var targetElement = document.querySelector(':target')
+    window.scroll({top: targetElement.offsetTop - navbarHeight})
+  }
+
+  window.addEventListener('hashchange', scrollWithOffset, false)
+  window.addEventListener('DOMContentLoaded', scrollWithOffset, false)
+}())
